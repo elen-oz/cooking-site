f20515282322fc86bfce3755ca9ac5faf6036b25
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-12 19:47:31 +0200

    feat: add post to create page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { useFetch } from '../../hooks/useFetch';
 
 //styles
 import './Create.css';
@@ -11,9 +12,11 @@ const Create = () => {
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
 
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime, ingredients);
+    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' });
   };
 
   const handleAdd = (e) => {
