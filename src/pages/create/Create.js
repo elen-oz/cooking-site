58d0ea21d26edf2aca3194c08c0098b60b60f0f8
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-12 19:26:37 +0200

    feat: add working form to recipe page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 //styles
 import './Create.css';
@@ -8,10 +7,23 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const [newIngredient, setNewIngredient] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+    setNewIngredient('');
+    ingredientInput.current.focus();
   };
 
   return (
@@ -25,6 +37,26 @@ const Create = () => {
         </label>
 
         {/* ingredients go here */}
+        <label>
+          <span>Recipe ingredients</span>
+          <div className='ingredients'>
+            <input
+              type='text'
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className='btn'>
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingrediets:{' '}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
 
         <label>
           <span>Recipe method:</span>
