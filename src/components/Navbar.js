ba37936767acccf7c6ae1c6f206fe47f93c0e0fb
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-17 18:15:00 +0200

    feat: add color theme selector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ import './Navbar.css';
 import Searchbar from './Searchbar';
 
 const Navbar = () => {
-  const { color, changeColor } = useTheme();
+  const { color } = useTheme();
 
   return (
     <div className='navbar' style={{ background: color }}>
-      <nav onClick={() => changeColor('pink')}>
+      <nav>
         <Link to='/' className='brand'>
           <h1>Grin and Baste It</h1>
         </Link>
