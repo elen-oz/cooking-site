14085a6c69aaf576ad0c5b55ce26479525ed033b
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-17 18:46:39 +0200

    feat: add light-dark mode selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTheme } from '../hooks/useTheme';
+import modeIcon from '../assets/mode-icon.svg';
 
 // styles
 import './ThemeSelector.css';
@@ -7,10 +8,23 @@ import './ThemeSelector.css';
 const themeColors = ['#58249c', '#249c6b', '#b70233'];
 
 const ThemeSelector = () => {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark');
+  };
 
   return (
     <div className='theme-selector'>
+      <div className='mode-toggle'>
+        <img
+          src={modeIcon}
+          onClick={toggleMode}
+          alt='light/dark toggle icon'
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+        />
+      </div>
+
       <div className='theme-buttons'>
         {themeColors.map((color) => (
           <div key={color} onClick={() => changeColor(color)} style={{ background: color }} />
