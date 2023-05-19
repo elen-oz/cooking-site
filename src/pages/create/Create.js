07740fa1ce5cc2a57e5c9f736dfccd5bb5a71d31
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-19 12:54:35 +0200

    feat: adding firestore data

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
 
 //styles
 import './Create.css';
@@ -14,11 +14,16 @@ const Create = () => {
   const ingredientInput = useRef(null);
   const history = useHistory();
 
-  const { postData, data } = useFetch('http://localhost:3000/recipes', 'POST');
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' });
+    const doc = { title, ingredients, method, cookingTime: cookingTime + ' minutes' };
+
+    try {
+      await projectFirestore.collection('recipes').add(doc);
+      history.push('/');
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleAdd = (e) => {
@@ -30,13 +35,6 @@ const Create = () => {
     setNewIngredient('');
     ingredientInput.current.focus();
   };
-
-  // redirect the user when we get data response
-  useEffect(() => {
-    if (data) {
-      history.push('/');
-    }
-  }, [data, history]);
 
   return (
     <div className='create'>
