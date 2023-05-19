470663dcb118a5f99cd7a6241c15a250eaf16001
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-19 12:43:17 +0200

    feat: fetch firestore document

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import { useFetch } from '../../hooks/useFetch';
 import { projectFirestore } from '../../firebase/config';
 
 // styles
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
+
 // styles
 import './Recipe.css';
 import { useTheme } from '../../hooks/useTheme';
 
 const Recipe = () => {
   const { id } = useParams();
-  const url = 'http://localhost:3000/recipes/' + id;
-  const { error, isPending, data: recipe } = useFetch(url);
   const { mode } = useTheme();
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError('Could not find that recipe');
+        }
+      });
+  }, [id]);
 
   return (
     <div className={`recipe ${mode}`}>
